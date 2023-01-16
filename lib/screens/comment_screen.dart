e15195a5cf20417db8605e0ea4a366767b8cc5b5
YusufBozTask <boz35yusuf@gmail.com>
2023-01-16 12:56:24 +0300

    show comment in post

--- lib/screens/comment_screen.dart
+++ lib/screens/comment_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:myband_flutter/models/user.dart';
 import 'package:myband_flutter/providers/user_provider.dart';
@@ -33,7 +34,26 @@ class _CommentScreenState extends State<CommentScreen> {
         title: const Text("Comments"),
         centerTitle: false,
       ),
-      body: CommentCard(),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('posts')
+            .doc(widget.snap['postId'])
+            .collection('comments')
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+            itemCount: (snapshot.data! as dynamic).docs.length,
+            itemBuilder: (context, index) => CommentCard(
+                // snap: snapshot.data!.docs[index],
+                ),
+          );
+        },
+      ),
       bottomNavigationBar: SafeArea(
         child: Container(
           height: kToolbarHeight,
